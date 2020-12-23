ad58dd761fb990b08baaeb50ae449a281e8211de
Ivan Nikolsky <enty8080@gmail.com>
2020-12-23 15:24:22 +0100

    Update ascy.py

--- ascy.py
+++ ascy.py
@@ -1,10 +1,16 @@
 import sys
 
-
 if len(sys.argv) < 2:
     print("Usage: ascy <input_file>")
     sys.exit()
-    
+
+print("Reading input file...")
 fout = open(sys.argv[1], 'rb').read().decode("unicode_escape")
+
+print("Formatting input file...")
 fin = open(sys.argv[1], 'wb')
+
+print("Outputting formated file...")
 fin.write(fout.encode())
+
+# you should believe that this code works.
